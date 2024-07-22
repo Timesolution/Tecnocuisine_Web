18dbb6a7609ea98ee3d203e0f50560a255269144
JuanReyTS <jcrey@timesolution.com.ar>
2024-07-22 14:44:39 -0300

    Listado de tipos de receta

---
+++ Tecnocuisine/Formularios/Maestros/TiposReceta.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Tecnocuisine_API.Controladores;
+using Tecnocuisine_API.Entitys;
+
+namespace Tecnocuisine.Formularios.Maestros
+{
+    public partial class TiposReceta : System.Web.UI.Page
+    {
+        ControladorTiposDeReceta cTiposDeReceta = new ControladorTiposDeReceta();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CargarListado();
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void CargarListado()
+        {
+            try
+            {
+                var tipos = cTiposDeReceta.ObtenerTodosTiposDeReceta();
+
+                if (tipos.Count > 0)
+                {
+                    foreach (var tipo in tipos)
+                    {
+                        CargarPH(tipo);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void CargarPH(TiposDeReceta tipo)
+        {
+            try
+            {
+                //fila
+                TableRow tr = new TableRow();
+                tr.ID = tipo.id.ToString();
+
+                //Celdas
+                TableCell celNumero = new TableCell();
+                celNumero.Text = tipo.id.ToString();
+                celNumero.VerticalAlign = VerticalAlign.Middle;
+                celNumero.HorizontalAlign = HorizontalAlign.Left;
+                celNumero.Width = Unit.Percentage(20);
+                celNumero.Attributes.Add("style", "padding-bottom: 1px !important;");
+                tr.Cells.Add(celNumero);
+
+                TableCell celNombre = new TableCell();
+                celNombre.Text = tipo.tipo;
+                celNombre.VerticalAlign = VerticalAlign.Middle;
+                celNombre.HorizontalAlign = HorizontalAlign.Left;
+                celNombre.Width = Unit.Percentage(40);
+                celNombre.Attributes.Add("style", "padding-bottom: 1px !important;");
+                tr.Cells.Add(celNombre);
+
+                //agrego fila a tabla
+                TableCell celAccion = new TableCell();
+                LinkButton btnDetalles = new LinkButton();
+                btnDetalles.CssClass = "btn btn-xs";
+                btnDetalles.Style.Add("background-color", "transparent");
+                btnDetalles.Attributes.Add("data-toggle", "modal");
+                btnDetalles.Attributes.Add("title", "Editar");
+                btnDetalles.Attributes.Add("href", "#openModalEditar");
+                btnDetalles.ID = "btnSelec_" + tipo.id + "_";
+                btnDetalles.Text = "<span><i style='color:black;' class='fa fa-pencil'></i></span>";
+                btnDetalles.OnClientClick = "openModalEditar(" + tipo.id + ");";
+                celAccion.Controls.Add(btnDetalles);
+
+                Literal l2 = new Literal();
+                l2.Text = "&nbsp";
+                celAccion.Controls.Add(l2);
+
+                LinkButton btnEliminar = new LinkButton();
+                btnEliminar.ID = "btnEliminar_" + tipo.id;
+                btnEliminar.CssClass = "btn btn-xs";
+                btnEliminar.Style.Add("background-color", "transparent");
+                btnEliminar.Attributes.Add("data-toggle", "modal");
+                btnEliminar.Attributes.Add("title", "Eliminar");
+                btnEliminar.Attributes.Add("href", "#modalConfirmacion2");
+                btnEliminar.Text = "<span><i style='color:red' class='fa fa-trash - o'></i></span>";
+                btnEliminar.OnClientClick = "abrirdialog(" + tipo.id + ");";
+                celAccion.Controls.Add(btnEliminar);
+
+                celAccion.Width = Unit.Percentage(30);
+                celAccion.Attributes.Add("style", "padding-bottom: 1px !important;");
+                tr.Cells.Add(celAccion);
+
+                phTipos.Controls.Add(tr);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
---
+++ Tecnocuisine/Formularios/Maestros/TiposReceta.aspx.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Tecnocuisine.Formularios.Maestros
+{
+
+
+    public partial class TiposReceta
+    {
+
+        /// <summary>
+        /// Control UpdatePanel2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel2;
+
+        /// <summary>
+        /// Control phTipos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.PlaceHolder phTipos;
+
+        /// <summary>
+        /// Control txtDescripcionInsumo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDescripcionInsumo;
+
+        /// <summary>
+        /// Control btnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton btnGuardar;
+
+        /// <summary>
+        /// Control hiddenEditar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hiddenEditar;
+
+        /// <summary>
+        /// Control txtDescripcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDescripcion;
+
+        /// <summary>
+        /// Control btnEditarInsumo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton btnEditarInsumo;
+
+        /// <summary>
+        /// Control idInsumoEditar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField idInsumoEditar;
+
+        /// <summary>
+        /// Control btnEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEliminar;
+
+        /// <summary>
+        /// Control hiddenID.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hiddenID;
+    }
+}
