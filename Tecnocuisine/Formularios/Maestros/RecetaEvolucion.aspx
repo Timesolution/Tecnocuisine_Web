dc619dd30c4d084817e7a23d5f89e853deeaa23b
JuanReyTS <jcrey@timesolution.com.ar>
2024-08-23 19:14:20 -0300

    Pagina recetas evolucion

---
+++ Tecnocuisine/Formularios/Maestros/RecetaEvolucion.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="RecetaEvolucion.aspx.cs" Inherits="Tecnocuisine.Formularios.Maestros.RecetaEvolucion" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div class="wrapper wrapper-content">
+        <div class="container-fluid">
+            <div class="ibox float-e-margins">
+                <%--  <div class="ibox-title">
+                    <h5>Recetas</h5>
+                    <div class="ibox-tools">
+                        <a class="collapse-link">
+                            <i class="fa fa-chevron-up"></i>
+                        </a>
+                    </div>
+                </div>--%>
+                <div class="">
+
+                    <asp:UpdatePanel ID="UpdatePanel2" runat="server">
+                        <ContentTemplate>
+
+                            <div class="wrapper wrapper-content animated fadeInRight">
+
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="ibox float-e-margins">
+
+                                            <div class="ibox-content">
+                                                <div style="margin-left: 0px; margin-right: 0px;" class="row">
+                                                    <div class="col-md-10">
+
+                                                        <h1>
+                                                            <asp:Label runat="server" ID="lblDescripcion" Style="font-size: 2rem; font-weight: bold"></asp:Label>
+                                                        </h1>
+
+                                                        <div class="input-group m-b">
+                                                            <span class="input-group-addon"><i style='color: black;' class='fa fa-search'></i></span>
+                                                            <input type="text" id="txtBusqueda" placeholder="Busqueda..." class="form-control" style="width: 90%" />
+                                                        </div>
+
+                                                    </div>
+
+                                                </div>
+
+                                                <table class="table table-striped table-bordered table-hover " id="editable">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>Fecha del cambio</th>
+                                                            <th>Ingrediente</th>
+                                                            <th style="text-align: right">Nuevo Costo Ingrediente</th>
+                                                            <th style="text-align: right">Anterior Costo Ingrediente</th>
+                                                            <th style="text-align: right">Nuevo Costo Total Receta</th>
+                                                            <th style="text-align: right">Anterior Costo Total Receta</th>
+                                                            <%--<th></th>--%>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <asp:PlaceHolder ID="phIngredientes" runat="server"></asp:PlaceHolder>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                        </ContentTemplate>
+
+                    </asp:UpdatePanel>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ Tecnocuisine/Formularios/Maestros/RecetaEvolucion.aspx.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Tecnocuisine_API.Controladores;
+using Tecnocuisine_API.Entitys;
+
+namespace Tecnocuisine.Formularios.Maestros
+{
+    public partial class RecetaEvolucion : System.Web.UI.Page
+    {
+        ControladorReceta controladorReceta = new ControladorReceta();
+        ControladorEntregas controladorEntregas = new ControladorEntregas();
+        int idReceta;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString["id"] == null)
+                Response.Redirect("Recetas.aspx");
+
+            idReceta = Convert.ToInt32(Request.QueryString["id"]);
+
+            var receta = controladorReceta.ObtenerRecetaId(idReceta);
+
+            lblDescripcion.Text = receta.descripcion;
+
+            CrearTablaIngredientes();
+        }
+
+        private void CrearTablaIngredientes()
+        {
+            CargarFilasProductos();
+            //CargarFilasRecetas();
+
+            ////ingredientes / Recetas
+            //obtenerRecetasbyIdReceta(idReceta);
+        }
+
+        private void CargarFilasProductos()
+        {
+            //List<Tecnocuisine_API.Entitys.Productos> productos = controladorReceta.obteneProductosPadres(idReceta);
+
+            //foreach (var producto in productos)
+            //{
+            //    //Obtener entregas por id producto
+            //    List<Entregas_Productos> entregas_producto = controladorEntregas.ObtenerEntregasProductoByidProducto(producto.id);
+            //    foreach (Entregas_Productos entrega in entregas_producto)
+            //    {
+            //        InsertarFilaProducto(entrega);
+            //    }
+            //}
+
+            List<EvolucionCostos_Productos> evoluciones = new ControladorEvolucionCostos_Productos().GetAll();
+
+            foreach (var e in evoluciones)
+            {
+                InsertarFilaProducto(e);
+            }
+        }
+
+        public void InsertarFilaProducto(EvolucionCostos_Productos evolucion)
+        {
+            try
+            {
+                //fila
+                TableRow tr = new TableRow();
+                tr.ID = "Evolucion" + evolucion.Id.ToString();
+
+                ////Celdas
+                //TableCell celNumero = new TableCell();
+                //celNumero.Text = entrega.id.ToString();
+                //celNumero.VerticalAlign = VerticalAlign.Middle;
+                //celNumero.HorizontalAlign = HorizontalAlign.Right;
+                //celNumero.Attributes.Add("style", " text-align: right");
+                //tr.Cells.Add(celNumero);
+
+                TableCell celFecha = new TableCell();
+                celFecha.Text = evolucion.FechaCambio.ToString();
+                celFecha.VerticalAlign = VerticalAlign.Middle;
+                celFecha.HorizontalAlign = HorizontalAlign.Left;
+                tr.Cells.Add(celFecha);
+
+                TableCell celDescripcion = new TableCell();
+                celDescripcion.Text = evolucion.Productos.descripcion;
+                celDescripcion.VerticalAlign = VerticalAlign.Middle;
+                celDescripcion.HorizontalAlign = HorizontalAlign.Left;
+                tr.Cells.Add(celDescripcion);
+
+                TableCell celCostoAnteriorIngrediente = new TableCell();
+                celCostoAnteriorIngrediente.Text = evolucion.CostoAnterior_Producto.ToString("C").Replace(',', '.');
+                celCostoAnteriorIngrediente.VerticalAlign = VerticalAlign.Middle;
+                celCostoAnteriorIngrediente.HorizontalAlign = HorizontalAlign.Left;
+                celCostoAnteriorIngrediente.Attributes.Add("style", "padding-bottom: 1px !important; text-align: right;");
+                tr.Cells.Add(celCostoAnteriorIngrediente);
+
+                TableCell celCostoNuevoIngrediente = new TableCell();
+                celCostoNuevoIngrediente.Text = evolucion.CostoNuevo_Producto.ToString("C").Replace(',', '.');
+                celCostoNuevoIngrediente.VerticalAlign = VerticalAlign.Middle;
+                celCostoNuevoIngrediente.HorizontalAlign = HorizontalAlign.Left;
+                celCostoNuevoIngrediente.Attributes.Add("style", "padding-bottom: 1px !important; text-align: right;");
+                tr.Cells.Add(celCostoNuevoIngrediente);
+
+                TableCell celCostoTotalAnteriorReceta = new TableCell();
+                celCostoTotalAnteriorReceta.Text = evolucion.CostoTotalAnterior_Receta.ToString("C").Replace(',', '.');
+                celCostoTotalAnteriorReceta.VerticalAlign = VerticalAlign.Middle;
+                celCostoTotalAnteriorReceta.HorizontalAlign = HorizontalAlign.Left;
+                celCostoTotalAnteriorReceta.Attributes.Add("style", "padding-bottom: 1px !important; text-align: right;");
+                tr.Cells.Add(celCostoTotalAnteriorReceta);
+
+                TableCell celCostoTotalNuevoReceta = new TableCell();
+                celCostoTotalNuevoReceta.Text = evolucion.CostoTotalNuevo_Receta.ToString("C").Replace(',', '.');
+                celCostoTotalNuevoReceta.VerticalAlign = VerticalAlign.Middle;
+                celCostoTotalNuevoReceta.HorizontalAlign = HorizontalAlign.Left;
+                celCostoTotalNuevoReceta.Attributes.Add("style", "padding-bottom: 1px !important; text-align: right;");
+                tr.Cells.Add(celCostoTotalNuevoReceta);
+
+                phIngredientes.Controls.Add(tr);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+    }
+}
